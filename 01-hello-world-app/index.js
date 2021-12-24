00fc400e60965167ca295b3fe1b0a3a260085687
natannegara <natannegara@gmail.com>
2021-12-24 20:01:34 +0700

    Update index.js

--- 01-hello-world-app/index.js
+++ 01-hello-world-app/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!, how are you doing my friend ?`);
+  res.send(`Hello ${name}!, how are you tonight ?`);
 });
 
 const port = process.env.PORT || 8080;
